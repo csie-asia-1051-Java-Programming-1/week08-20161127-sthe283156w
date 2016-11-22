f0573edac141dd22e9afdb3c27184dadcee89254
sthe283156w <sthe283156w@icloud.com>
2016-11-22 19:52:15 +0800

    Update ex01.java

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -9,7 +9,15 @@ public class ex01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		System.out.println(fun(n));
 
+	}
+	public static String fun(int v1){
+		if(v1/2==0)
+			return Integer.toString(v1%2);
+		return fun(v1/2)+(v1%2);
 	}
 
 }
